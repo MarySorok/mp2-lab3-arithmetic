76fcd56976ae153de62fe975e93233085a61d548
MarySorok <32310821+MarySorok@users.noreply.github.com>
2017-11-08 19:17:46 +0300

    версия 1
    прописала  классы и почти весь стек

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,41 @@
 // объ€вление функций и классов дл€ вычислени€ арифметических выражений
+#include <cstring>
+#include <cstdlib>
+#include  <ctype>
+#include "stack.h"
+#include <iostream>
+
+using namespace std;
+typedef enum Lextype {op_br,cl_br,var,val,oper};
+
+struct Lexem // лексем
+{
+	char str[20];
+	Lextype type;
+public:
+	Lexem(char*s, Lextype t);
+	Lexem(const Lexem &l);
+	Lexem& operator =(const Lexem&l);
+	bool operator ==(const Lexem&l);
+};
+struct Variable // переменна€ и ее значение
+{
+	char name[10];
+	double value;
+};
+class Arithmetic
+{
+	char* inputstr;
+	Lexem* lexems;
+	int nLex;
+	Lexem* polish;
+	int np;
+	Variable*vars;
+	int nvars;
+public:
+	Arithmetic(char*s);
+	int check();
+	void print_polish();
+	void set_vars();
+	double calculate();
+};
--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,67 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+
+template <class ValType>
+class Stack
+{
+	ValType *p;
+	int topindex;
+	int size;
+public:
+	Stack(int s=1000);
+	~Stack();
+	void push(ValType a);
+	ValType pop();
+	ValType top();
+	bool isempty();
+	void clean();
+	int getquantity() { return topindex; }
+};
+
+template <class ValType>
+Stack<ValType>::Stack(int s= 1000)
+{
+	topindex = 0;
+	size = s;
+	p = new ValType[s];
+}
+template <class ValType>
+Stack<ValType>::~Stack()
+{
+	delete[]p;
+}
+template <class ValType>
+void Stack<ValType>::push(ValType a)
+{
+	// здесь что делаем, если стек переполнен
+	p[topindex] = a;
+	topindex += 1;	
+}
+template <class ValType>
+void Stack<ValType>::clean()
+{
+	//возможно, нужно удалить все значения, которые лежали в стеке
+	topindex = 0;
+}
+template <class ValType>
+bool Stack<ValType>::isempty()
+{
+	if (topindex == 0)
+		return true;
+}
+template <class ValType>
+ValType Stack<ValType>::pop()
+{
+	if (topindex == 0)
+		throw ("Error. Stack is empty");
+	topindex -= 1;
+	return p[topindex];
+}
+template <class ValType>
+ValType Stack<ValType>::top()
+{
+	if (topindex == 0)
+		throw ("Error. Stack is empty");
+	return p[topindex-1];
+}
